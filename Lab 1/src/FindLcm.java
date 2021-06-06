9ba88e492cf8935ce63a4cc9c46df33e75e451d9
Ummer Farooq <Ummer Farooq@Ummer>
2021-06-06 15:06:28 +0530

    first commit

---
+++ Lab 1/src/FindLcm.java
@@ -0,0 +1,20 @@
+
+public class FindLcm {
+	
+		 public static void main(String[] args) {
+		 int n1 = 6, n2 = 4, lcm;
+		 // maximum number between n1 and n2 is stored in lcm
+		 lcm = (n1 > n2) ? n1 : n2;
+		// Always true
+		 while(true) {
+		 if( lcm % n1 == 0 && lcm % n2 == 0 ) {
+		 System.out.printf("The LCM of %d and %d is %d.", n1,
+		n2, lcm);
+		 break;
+		 }
+		 ++lcm;
+		 }
+		 }
+    }
+
+
